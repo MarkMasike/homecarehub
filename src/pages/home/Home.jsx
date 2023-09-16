8c4e6f463665f19024d631295db37899f1ae106c
MarkMasike <marksoko002@gmail.com>
2023-09-17 00:22:03 +0300

    Category Slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Featured from '../../components/featuted/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
-
+import Slide from '../../components/slide/Slide'
+import {cards} from "../../data"
+import CatCard from '../../components/catCard/CatCard'
 const Home = () => {
   return (
    <div className="home">
       <Featured/>
       <TrustedBy/>
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
    </div>
   )
 }
